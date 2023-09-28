aef0440b548b5cf1662d56a16fcfb32ebd69fdab
Gabriela S Santos <gbss.santos@poli.ufrj.br>
2023-09-28 11:42:57 -0300

    questao 1 ad2

---
+++ AD2-gabriela-santos/paginas-html5/index.js
@@ -0,0 +1,21 @@
+// ----------- Questão 1 ----------
+
+function abrir_janela_miniMe(){
+    window.open("janela_minime.html","_blank","width=400, height=415");
+}
+
+function abrir_janela_genioSPlus(){
+    window.open("janela_geniosplus.html","_blank","width=400, height=415");
+}
+
+function abrir_janela_inissia(){
+    window.open("janela_inissia.html","_blank","width=400, height=415");
+}
+
+function abrir_janela_lov(){
+    window.open("janela_lov.html","_blank","width=400, height=415");
+}
+
+function fechar_janela(){
+    window.close();
+}
