6789e95f05c91f09d79bc120d045398bd7519db0
Gabriela Santos <gbss.santos@poli.ufrj.br>
2023-10-13 00:57:22 -0300

    questao 4 e questao 5

--- AD2-gabriela-santos/paginas-html5/index.js
+++ AD2-gabriela-santos/paginas-html5/index.js
@@ -58,18 +58,102 @@ function alterar_descricao_capsulas(array_capsulas){
     console.log(href_imagem);
 }
 
-// ----------- Questão 3 ----------
+// ----------- Questão 4 ----------
 
-var add_qtd_maq = 0 
+function multiplicacao(val1, val2) {
+    return parseInt(val1) * parseInt(val2);
+}
+
+var add_qtd_maq = 0;
+var valor_maquina = 0;
 function adicionar_qtd_maq(){
     if (add_qtd_maq < 5) {
         document.getElementById('idQtdMaquina').value = ++add_qtd_maq;
     }
+    valor_maquina = document.getElementById('selMaquina').value;
+    var multiplicacao_maq = multiplicacao(valor_maquina, add_qtd_maq);
+    document.getElementById('idValUnitario').value = multiplicacao_maq;
 }
 
-var add_qtd_cap = 0 
+function add_um_qtd_maq(valor){
+    add_qtd_maq = valor;
+    adicionar_qtd_maq()
+    document.getElementById('selCapsula').value = "-----";
+}
+
+var add_qtd_cap = 0;
+var valor_capsula = 0;
 function adicionar_qtd_cap(){
     if (add_qtd_cap < 30) {
         document.getElementById('idQtdCapsula').value = ++add_qtd_cap;
     }
+    valor_capsula = document.getElementById('selCapsula').value;
+    var multiplicacao_cap = multiplicacao(valor_capsula,add_qtd_cap);
+    document.getElementById('idValUnitario').value = multiplicacao_cap;
+}
+
+function add_um_qtd_cap(valor){
+    add_qtd_cap = valor;
+    adicionar_qtd_cap();
+    document.getElementById('selMaquina').value = "-----";
+}
+
+// ----------- Questão 5 ----------
+
+var add_valor_total = 0;
+
+function adicionar_a_lista_compras_maquinas(){
+    var elemento_maquina = document.getElementById('selMaquina');
+    var nome_maquina = elemento_maquina.options[elemento_maquina.selectedIndex].text
+    var total = document.getElementById('idValUnitario').value;
+    
+    if (nome_maquina === "-----"){
+        alert("Nenhum produto selecionado !")
+        document.getElementById('idQtdMaquina').value = "";
+        return
+    }
+
+    let textarea = document.querySelector(".boxPedidos");
+    let texto = `${nome_maquina} (${add_qtd_maq}) R$ ${total},00\n`;
+    textarea.value += texto;
+
+    document.getElementById('selMaquina').value = "-----";
+    document.getElementById('idQtdMaquina').value = 1;
+    document.getElementById('idValUnitario').value = 1;
+    add_qtd_maq = 0;
+
+    valor_total(total);
+}
+
+function adicionar_a_lista_compras_capsulas(){
+    var elemento_capsula = document.getElementById('selCapsula');
+    var nome_capsula = elemento_capsula.options[elemento_capsula.selectedIndex].text
+    var total = document.getElementById('idValUnitario').value;
+    
+    if (nome_capsula === "-----"){
+        alert("Nenhum produto selecionado !")
+        document.getElementById('idQtdCapsula').value = "";
+        return
+    }
+    
+    let textarea = document.querySelector(".boxPedidos");
+    let texto = `${nome_capsula} (${add_qtd_cap}) R$ ${total},00\n`;
+    textarea.value += texto;
+
+    document.getElementById('selCapsula').value = "-----";
+    document.getElementById('idQtdCapsula').value = 1;
+    document.getElementById('idValUnitario').value = 1;
+    add_qtd_cap = 0;
+
+    valor_total(total);
+}
+
+function valor_total(valor){
+    add_valor_total += parseInt(valor);
+    document.getElementById("idValTotal").value = add_valor_total;
+}
+
+function limpar_valor_total(){
+    add_valor_total = 0;
+    document.getElementById('idValUnitario').value = 0;
 }
